096790a0a9ce8f635e5faa33cdacacd820084961
abc20210708 <gotqlc2670@naver.com>
2022-09-27 18:42:13 +0900

    소수 만들기

---
+++ src/main/java/com/example/programmers/lv1/Q10.java
@@ -0,0 +1,39 @@
+package com.example.programmers.lv1;
+//참고 블로그 https://blog.itcode.dev/posts/2021/12/14/programmers-a0009.html
+public class Q10 {
+
+        public int solution(int[] nums) {
+            int answer = 0;
+            int num = 0;
+
+            for(int i = 0; i < nums.length; i++) {
+                for(int j = i+1; j < nums.length; j++) {
+                    for(int k = j+1; k < nums.length; k++) {
+                        num = (nums[i] + nums[j] + nums[k]);
+                        System.out.println(num);
+
+                        answer += isPrime(num) ? 1 : 0;
+
+                    }//k
+                }//j
+            }//i
+
+
+            System.out.println("answer: "+answer);
+            return answer;
+        }
+
+        //소수 여부 변환 메서드
+        private boolean isPrime(int num) {
+            for(int i = 2; i <= Math.sqrt(num); i++) {
+                //나눠 떨어지는 경우
+                if(num % i == 0) return false;
+            }
+            return true;
+        }
+
+
+    }
+
+
+
