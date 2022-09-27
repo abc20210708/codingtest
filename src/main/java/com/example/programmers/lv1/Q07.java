e36036ecfac46ecda3aca68147841d39a9dfa332
abc20210708 <gotqlc2670@naver.com>
2022-09-27 19:51:07 +0900

    Update Q07.java

--- src/main/java/com/example/programmers/lv1/Q07.java
+++ src/main/java/com/example/programmers/lv1/Q07.java
@@ -51,6 +51,30 @@ s	                    result
 "123"	                123
 * */
 
+/* 다른풀이
+class Solution {
+    public int solution(String s) {
+
+        String[][] mapArr = { {"0", "zero"},
+                              {"1", "one"},
+                              {"2", "two"},
+                              {"3", "three"},
+                              {"4", "four"},
+                              {"5", "five"},
+                              {"6", "six"},
+                              {"7", "seven"},
+                              {"8", "eight"},
+                              {"9", "nine"} };
+
+        for(String[] map : mapArr){
+            s = s.replace(map[1], map[0]);
+        }
+
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+* */
 
 /* 다른풀이
 import java.util.*;
@@ -72,3 +96,4 @@ class Solution {
 }
 
 * */
+
