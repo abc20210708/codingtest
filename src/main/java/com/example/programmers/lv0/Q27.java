649786d34a0dbf17d5e38138d463f99cd3338512
abc20210708 <gotqlc2670@naver.com>
2022-10-11 14:43:52 +0900

    Update Q27.java

--- src/main/java/com/example/programmers/lv0/Q27.java
+++ src/main/java/com/example/programmers/lv0/Q27.java
@@ -1,7 +1,45 @@
 package com.example.programmers.lv0;
 
 public class Q27 {
+    class Solution {
+        public int solution(int hp) { //5  3  1
+            int answer = 0;
+
+            while(hp > 0) {
+                if(hp >= 5) {
+                    answer += (hp / 5);
+                    hp %= 5;
+                } else if (hp >= 3) {
+                    answer += (hp / 3);
+                    hp %= 3;
+                }  else if (hp >= 1) {
+                    answer += (hp / 1);
+                    hp %= 1;
+                }
+
+            }
+
+            return answer;
+        }
+    }
 }
+
+/* 다른 풀이
+class Solution {
+    public int solution(int hp) {
+        int answer = hp / 5;
+        hp %= 5;
+
+        answer += hp / 3;
+        hp %= 3;
+
+        answer += hp / 1;
+
+        return answer;
+    }
+}
+
+* */
 /*
 개미 군단
 문제 설명
