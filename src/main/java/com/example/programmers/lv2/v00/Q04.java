b55b7ff439669b0cdec3f19e9d3c5c25ed84c9a7
abc20210708 <gotqlc2670@naver.com>
2022-10-21 17:11:04 +0900

    Update Q04.java

--- src/main/java/com/example/programmers/lv2/v00/Q04.java
+++ src/main/java/com/example/programmers/lv2/v00/Q04.java
@@ -25,6 +25,22 @@ public class Q04 {
     }
 }
 
+/* 다른 풀이
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i=0; i<citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length-i);
+            answer = Math.max(answer, smaller);
+        }
+        return answer;
+    }
+}
+* */
+
 /*
 H-Index
 
