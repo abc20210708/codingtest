c8c3b26ecc82209818f2c46868bd84154b488f43
abc20210708 <gotqlc2670@naver.com>
2022-11-07 20:41:39 +0900

    [SQL] lv1. 아픈 동물 찾기

---
+++ src/main/java/com/example/sqlkit/lv01/Q01.java
@@ -0,0 +1,12 @@
+package com.example.sqlkit.lv01;
+
+//lv01. 아픈 동물 찾기
+public class Q01 {
+    /*
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID;
+     */
+}
