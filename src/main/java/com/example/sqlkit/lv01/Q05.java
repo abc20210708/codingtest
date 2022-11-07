922c98af47d74b5f9040b2a00d871ce16fffa89a
abc20210708 <gotqlc2670@naver.com>
2022-11-07 21:08:00 +0900

    [SQL] lv1. 강원도에 위치한 생산공장 목록 출력하기

---
+++ src/main/java/com/example/sqlkit/lv01/Q05.java
@@ -0,0 +1,11 @@
+package com.example.sqlkit.lv01;
+
+//lv1. 강원도에 위치한 생산공장 목록 출력하기
+public class Q05 {
+    /*
+    SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+    FROM FOOD_FACTORY
+    WHERE ADDRESS LIKE '강원도%'
+    ORDER BY FACTORY_ID;
+    * */
+}
