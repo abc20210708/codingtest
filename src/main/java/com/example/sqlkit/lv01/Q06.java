6b9e4472fbd8ce7b72a4c0de6d61eeb6e78f7ebb
abc20210708 <gotqlc2670@naver.com>
2022-11-20 21:58:46 +0900

    [SQL] lv1. 이름이 없는 동물의 아이디

--- src/main/java/com/example/sqlkit/lv01/Q06.java
+++ src/main/java/com/example/sqlkit/lv01/Q06.java
@@ -3,6 +3,9 @@ package com.example.sqlkit.lv01;
 //lv1. 이름이 없는 동물의 아이디
 public class Q06 {
     /*
-
+    SELECT ANIMAL_ID
+    FROM ANIMAL_INS
+    WHERE NAME IS NULL
+    ORDER BY ANIMAL_ID
     * */
 }
