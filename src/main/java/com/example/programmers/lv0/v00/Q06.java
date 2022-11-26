7717d067e33abc5d7aaea2b1d2a2a4bc35bec585
abc20210708 <gotqlc2670@naver.com>
2022-11-26 21:39:10 +0900

    Update Q06.java

--- src/main/java/com/example/programmers/lv0/v00/Q06.java
+++ src/main/java/com/example/programmers/lv0/v00/Q06.java
@@ -18,6 +18,20 @@ public class Q06 {
 
 }
 
+/* 다른 풀이
+class Solution {
+    public long solution(String numbers) {
+        String[] alphas = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] nums = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        for (int i = 0; i < 10; i++) {
+            numbers = numbers.replaceAll(alphas[i], nums[i]);
+        }
+
+        return Long.parseLong(numbers);
+    }
+}
+* */
 
 /*
 영어가 싫어요
