b0b2f9f702f7d6a04c89448b13b77c1a6bda6a94
abc20210708 <gotqlc2670@naver.com>
2022-11-26 18:08:33 +0900

    Update Q02.java

--- src/main/java/com/example/programmers/lv0/v00/Q02.java
+++ src/main/java/com/example/programmers/lv0/v00/Q02.java
@@ -23,6 +23,21 @@ public class Q02 {
     }
 }
 
+/* 다른 풀이
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = Integer.toString(n);
+        String[] arr = str.split("");
+
+        for(int i = 0; i < arr.length; i++) {
+            answer += Integer.parseInt(arr[i]);
+        }
+        return answer;
+    }
+}
+* */
+
 /*
 자릿수 더하기
 
