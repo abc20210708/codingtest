eefb05909dcca24e0f468627624d9551500c14da
abc20210708 <gotqlc2670@naver.com>
2022-11-28 19:22:08 +0900

    Update Q14.java

--- src/main/java/com/example/programmers/lv0/v10/Q14.java
+++ src/main/java/com/example/programmers/lv0/v10/Q14.java
@@ -17,6 +17,22 @@ public class Q14 {
     }
 }
 
+/* 다른 풀이
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+
+        for(String str1: s1) {
+            for(String str2: s2) {
+                if(str1.equals(str2)) answer++;
+            }
+        }
+
+        return answer;
+    }
+}
+* */
+
 /*
 배열의 유사도
 
