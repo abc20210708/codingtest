f8ede181df00915e28812b3b242f82e90eb1e301
abc20210708 <gotqlc2670@naver.com>
2022-11-29 17:51:16 +0900

    Update Q19.java

--- src/main/java/com/example/programmers/lv0/v10/Q19.java
+++ src/main/java/com/example/programmers/lv0/v10/Q19.java
@@ -57,6 +57,23 @@ class Solution {
 }
 * */
 
+/* 다른 풀이 3
+ class Solution {
+    public int solution(int order) {
+        int answer = 0;
+
+        String str = order+"";
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(c=='3'||c=='6'||c=='9') answer++;
+        }
+
+        return answer;
+    }
+}
+* */
+
 
 /*
 369게임
