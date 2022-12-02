d72feb7c04429dbe08cd06e35c2019caa1c77daa
abc20210708 <gotqlc2670@naver.com>
2022-12-02 20:28:30 +0900

    [SQL] lv1. 경기도에 위치한 식품창고 목록 출력하기

---
+++ src/main/java/com/example/sqlkit/lv01/Q07.java
@@ -0,0 +1,13 @@
+package com.example.sqlkit.lv01;
+
+//lv1. 경기도에 위치한 식품창고 목록 출력하기
+public class Q07 {
+    /*
+    SELECT WAREHOUSE_ID, WAREHOUSE_NAME,
+    ADDRESS,
+    CASE WHEN FREEZER_YN IS NULL THEN 'N' ELSE FREEZER_YN END
+    FROM FOOD_WAREHOUSE
+    WHERE WAREHOUSE_NAME LIKE '%경기%'
+    * */
+}
+
