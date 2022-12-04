5a32fff7fa34cace5d24c9b1cd48c98706ccc58f
abc20210708 <gotqlc2670@naver.com>
2022-12-04 20:38:29 +0900

    [SQL] lv1. 조건에 맞는 회원수 구하기

---
+++ src/main/java/com/example/sqlkit/lv01/Q09.java
@@ -0,0 +1,11 @@
+package com.example.sqlkit.lv01;
+
+//lv1. 조건에 맞는 회원수 구하기
+public class Q09 {
+    /*
+     SELECT COUNT(USER_ID) AS USERS
+    FROM USER_INFO
+    WHERE JOINED LIKE '2021%'
+    AND 20 <= AGE AND AGE <=29
+    * */
+}
