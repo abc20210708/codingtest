1e48778fde85dbb9f20681318f7c25bf1603153e
abc20210708 <gotqlc2670@naver.com>
2022-12-05 18:01:32 +0900

    Update Q14.java

--- src/main/java/com/example/programmers/lv1/v10/Q14.java
+++ src/main/java/com/example/programmers/lv1/v10/Q14.java
@@ -63,6 +63,23 @@ class Solution {
 }
 */
 
+/* 댜른 풀이 3
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        String answer = participant[participant.length-1];
+
+        for(int i=0;i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                answer=participant[i];
+                break;
+            }
+        }
+        return answer;
+    }
+}
+* */
 
 /*
 완주하지 못한 선수
