6d95d43e48e32b5193450f514e22292bbe836ae8
abc20210708 <gotqlc2670@naver.com>
2022-12-05 18:59:11 +0900

    Update Q14.java

--- src/main/java/com/example/programmers/lv1/v10/Q14.java
+++ src/main/java/com/example/programmers/lv1/v10/Q14.java
@@ -81,6 +81,39 @@ class Solution {
 }
 * */
 
+/* 다른 풀이 4
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String p: participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for(String c: completion) {
+            map.put(c, map.get(c) -1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()) {
+            Map.Entry<String, Integer> entry = iter.next();
+            if(entry.getValue() != 0) {
+                answer = entry.getKey();
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+
+* */
+
 /*
 완주하지 못한 선수
 문제 설명
