eed077fa064e7309ed20cdca084a386c1d61be0b
abc20210708 <gotqlc2670@naver.com>
2022-12-12 12:13:27 +0900

    [SQL] lv1. 인기있는 아이스크림

---
+++ src/main/java/com/example/sqlkit/lv10/Q12.java
@@ -0,0 +1,12 @@
+package com.example.sqlkit.lv10;
+
+//lv1. 인기있는 아이스크림
+public class Q12 {
+    /*
+
+SELECT FLAVOR
+FROM FIRST_HALF
+ORDER BY TOTAL_ORDER DESC,
+        SHIPMENT_ID ASC
+    * */
+}
