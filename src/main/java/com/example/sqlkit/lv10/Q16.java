674a19d4ea50721ea8986dfe4ee41af1dbcf8585
abc20210708 <gotqlc2670@naver.com>
2022-12-16 12:11:56 +0900

    [SQL] lv1. 조건에 맞는 도서 리스트 출력하기

---
+++ src/main/java/com/example/sqlkit/lv10/Q16.java
@@ -0,0 +1,13 @@
+package com.example.sqlkit.lv10;
+
+//lv1. 조건에 맞는 도서 리스트 출력하기
+public class Q16 {
+    /*
+    SELECT BOOK_ID,
+    TO_CHAR(PUBLISHED_DATE, 'yyyy-MM-dd') as PUBLISHED_DATE
+    FROM BOOK
+    where TO_CHAR(PUBLISHED_DATE, 'yyyy') = '2021'
+    AND CATEGORY = '인문'
+    ORDER BY PUBLISHED_DATE
+    * */
+}
