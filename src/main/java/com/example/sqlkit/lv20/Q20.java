2eea045ea1c82a0cab05c87d84adccab42fb032a
abc20210708 <gotqlc2670@naver.com>
2022-12-20 16:57:13 +0900

    [SQL] lv2. 이름에 el이 들어가는 동물 찾기

---
+++ src/main/java/com/example/sqlkit/lv20/Q20.java
@@ -0,0 +1,13 @@
+package com.example.sqlkit.lv20;
+
+//lv2. 이름에 el이 들어가는 동물 찾기
+public class Q20 {
+    /*
+
+    SELECT ANIMAL_ID, NAME
+    FROM ANIMAL_INS
+    WHERE ANIMAL_TYPE = 'Dog'
+    AND NAME LIKE '%EL%'
+    ORDER BY NAME
+    * */
+}
