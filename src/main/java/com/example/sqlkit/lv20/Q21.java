10000c9615ad32b09a222307707ea590b0b59a20
abc20210708 <gotqlc2670@naver.com>
2022-12-21 12:12:48 +0900

    [SQL] lv2. 동명 동물 수 찾기

---
+++ src/main/java/com/example/sqlkit/lv20/Q21.java
@@ -0,0 +1,12 @@
+package com.example.sqlkit.lv20;
+
+//lv2. 동명 동물 수 찾기
+public class Q21 {
+    /*
+     SELECT NAME, COUNT(NAME)
+    FROM ANIMAL_INS
+    GROUP BY NAME
+    HAVING COUNT(NAME) >= 2
+    ORDER BY NAME
+    * */
+}
