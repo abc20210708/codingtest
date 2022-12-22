80e4c1f8e70c4f77e79f1daec58a04c74dfec665
abc20210708 <gotqlc2670@naver.com>
2022-12-23 06:45:47 +0900

    [SQL] lv2. DATETIME에서 DATE로 형 변환

---
+++ src/main/java/com/example/sqlkit/lv20/Q23.java
@@ -0,0 +1,10 @@
+package com.example.sqlkit.lv20;
+
+//lv2. DATETIME에서 DATE로 형 변환
+public class Q23 {
+    /*
+SELECT ANIMAL_ID, NAME, TO_CHAR(DATETIME, 'YYYY-MM-DD') AS 날짜
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+    * */
+}
