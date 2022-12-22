69f1cc288f7a77e80d8bc27e946c6c6dc8cfde13
abc20210708 <gotqlc2670@naver.com>
2022-12-22 15:19:13 +0900

    [SQL] lv2. NULL 처리하기

---
+++ src/main/java/com/example/sqlkit/lv20/Q22.java
@@ -0,0 +1,10 @@
+package com.example.sqlkit.lv20;
+
+//lv2. NULL 처리하기
+public class Q22 {
+    /*
+    SELECT ANIMAL_TYPE, NVL(NAME, 'No name') AS NAME, SEX_UPON_INTAKE
+    FROM ANIMAL_INS
+    ORDER BY ANIMAL_ID
+    * */
+}
