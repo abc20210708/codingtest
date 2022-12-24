1d100da7bbdca4f07d23f843c237c0a41d4cf3bb
abc20210708 <gotqlc2670@naver.com>
2022-12-24 18:50:26 +0900

    [SQL] lv2. 고양이와 개는 몇 마리 있을까

---
+++ src/main/java/com/example/sqlkit/lv20/Q24.java
@@ -0,0 +1,14 @@
+package com.example.sqlkit.lv20;
+
+//lv2. 고양이와 개는 몇 마리 있을까
+
+public class Q24 {
+    /*
+
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
+    * */
+}
