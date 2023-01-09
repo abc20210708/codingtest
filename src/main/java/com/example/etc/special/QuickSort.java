dfe0799a7bb5af907524fc33f48d9efe8b1fc437
abc20210708 <gotqlc2670@naver.com>
2023-01-09 20:44:42 +0900

    Update QuickSort.java

--- src/main/java/com/example/etc/special/QuickSort.java
+++ src/main/java/com/example/etc/special/QuickSort.java
@@ -2,44 +2,45 @@ package com.example.etc.special;
 
 public class QuickSort {
 
-    public void quickSort(int[] arr, int start, int end) {
+    public static void main(String[] args){
 
-        if(start >= end) //원소가 1개인 경우 종료
-            return;
+        int array[] = {5, 7, 9, 0, 3, 1, 6, 2, 4, 8};
 
-        int pivot = start; // 피벗은 첫 번째 원소
-        int left = start +1;
-        int right = end;
+        quickSort(array, 0, array.length -1);
 
-        while (left <= right) {
-
-            //피벗보다 큰 데이터를 찾을 때까지 반복
-            while(left <= end && arr[left] <= arr[pivot])
-                left++;
-
-            //피벗보다 작은 데이터를 찾을 때까지 반복
-            while(right > start && arr[right] >= arr[pivot])
-                right--;
-
-            if(left > right) { //엇갈렸다면 작은 데이터와 피벗을 교체
-                int temp = arr[right];
-                arr[right] = arr[pivot];
-                arr[pivot] = temp;
-            } else { //엇갈리지 않았다면 적은 데이터와 큰 데이터를 교체
-                int temp = arr[right];
-                arr[right] = arr[left];
-                arr[left] = temp;
-            }
-
-            //분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
-            quickSort(arr, start, right -1);
-            quickSort(arr, right + 1, end);
-
+        for (int i : array) {
+            System.out.printf("%d ",i);
         }
-
-
-
     }
 
+    public static int partition(int[] arr, int start, int end) {
 
+        int pivot = arr[(start + end) /2];
+
+        while(start < end) {
+            while ((arr[start] < pivot) && (start < end))
+                start++;
+            while((arr[end] > pivot) && (start < end))
+                end--;
+
+            if(start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+            }
+        }
+
+        return start;
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+
+        if(start < end) {
+            int pivotNewIdx = partition(arr, start, end);
+
+            quickSort(arr, start, pivotNewIdx -1);
+            quickSort(arr, pivotNewIdx +1, end);
+        }
+
+    }
 }
