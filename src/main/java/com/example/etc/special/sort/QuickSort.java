0b449299c2db5d3188020787b0ea6a65095f5f57
abc20210708 <gotqlc2670@naver.com>
2023-01-10 08:09:56 +0900

    Update QuickSort.java

--- src/main/java/com/example/etc/special/sort/QuickSort.java
+++ src/main/java/com/example/etc/special/sort/QuickSort.java
@@ -1,8 +1,58 @@
 package com.example.etc.special.sort;
+import java.util.Arrays;
 
 public class QuickSort {
-
+    private static void quickSort(int[] arr){
+        quickSort(arr, 0, arr.length-1);
+    }
+    private static void quickSort(int[] arr, int start, int end){
+        //오른쪽 파티션의 시작점
+        int part2 = partition(arr,start,end);
+        //시작점이 오른쪽 시작점보다 둘 이상 차이가 날 때만
+        //왼쪽 파티션 퀵소트
+        if(start < part2 -1){
+            quickSort(arr, start, part2-1);
+        }
+        //오른쪽 시작점이 끝점보다 작을 때
+        //오른쪽 파티션 퀵소트
+        if(part2<end){
+            quickSort(arr, part2, end);
+        }
+    }
+    private static int partition(int[] arr, int start, int end){
+        int pivot = arr[(start+end)/2];
+        while(start <= end){
+            while(arr[start] < pivot)
+                start++;
+            while(arr[end] > pivot)
+                end--;
+            if(start <= end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+    private static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+    private static void printArray(int[] arr){
+        for(int data : arr){
+            System.out.print(data + ", ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int[] arr = {3, 9, 4, 7, 5, 0, 1, 6, 8, 2};
+        printArray(arr);
+        quickSort(arr);
+        printArray(arr);
+    }
 }
+
 
 /* 퀵 정렬(QuickSort)은 중심점(pivot Point)을 임의로 고른 후, 이 중심점보다 작은 수를
     한 쪽으로 분류하고 큰 수들은 다른 쪽으로 분류하여 정렬하는 방법입니다.
