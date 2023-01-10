11d5daa0f1f2927a34ed9d0577b04280d8b452ae
abc20210708 <gotqlc2670@naver.com>
2023-01-10 16:42:00 +0900

    Create NumberCount_3.java

---
+++ src/main/java/com/example/etc/special/numbercount/NumberCount_3.java
@@ -0,0 +1,32 @@
+package com.example.etc.special.numbercount;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberCount_3 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        int[] arr = new int[10];
+
+        while(val != 0) {
+            arr[val % 10]++;
+            val /= 10;
+        }
+
+        for (int result: arr) {
+            System.out.println(result);
+        }
+    }
+}
+
+/*
+ 먼저 while 문의 조건으로 val이 0이 아니면 반복
+ 그리고 val % 10의 값
+ 즉, 나머지 값을 배열의 index로 활용하여 해당 인덱스의 값을 1 증가시켜주는 것이다.
+ 그런 뒤 val = val/10; 을 통해 한 자릿수씩 줄여나간다.
+
+* */
