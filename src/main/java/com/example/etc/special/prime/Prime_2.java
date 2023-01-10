feaced1b0b0b9706de7595b2c2b421e97c002583
abc20210708 <gotqlc2670@naver.com>
2023-01-10 14:16:49 +0900

    Update Prime_2.java

--- src/main/java/com/example/etc/special/prime/Prime_2.java
+++ src/main/java/com/example/etc/special/prime/Prime_2.java
@@ -1,6 +1,46 @@
 package com.example.etc.special.prime;
 
+import java.util.Scanner;
+
 public class Prime_2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        //0 ~ N까지 수 중 소수를 구하는 반복문
+        for(int i = 0; i <= N; i++) {
+            make_prime(i);
+        }
+    }
+
+    public static void make_prime(int num) {
+
+        //0과 1은 소수가 아니므로 종료
+        if(num < 2) return;
+
+        //2는 소수
+        if(num == 2) {
+            System.out.println(num);
+            return;
+        }
+
+        for (int i = 2; i < num; i++) {
+
+            //소수가 아닐 경우
+            if (num % i == 0) {
+                System.out.println("소수가 아닙니다.");
+                return;
+            }
+        }
+
+        //위 반복문에서 약수를 갖고 있지 않은 경우 소수
+        System.out.println("소수입니다.");
+        return;
+
+    }
+
 }
 
 /* N 이하의 모든 소수를 구하는 알고리즘
