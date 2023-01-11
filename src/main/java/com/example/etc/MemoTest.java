a06b3d21401bda6f0fcc10ff2c7f6f0f05731e7f
abc20210708 <gotqlc2670@naver.com>
2023-01-11 23:12:28 +0900

    Update MemoTest.java

--- src/main/java/com/example/etc/MemoTest.java
+++ src/main/java/com/example/etc/MemoTest.java
@@ -1,38 +1,56 @@
 package com.example.etc;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
 
-
-/*
-한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하라
-각 단어는 공백으로 구분된다.
-
-예시)  my name is daramG  -> daramG
-* */
-
+//
 public class MemoTest {
 
-    public String solution(String target) {
+    int[] arr = new int[100001]; //최댓값 100000
 
-        String result = "";
+    public int solution(int n) {
+        int result = 0;
 
-        String[] str = target.split(" ");
-
-        int max = 0;
-        for(String s : str) {
-            if(s.length() > max) result = s;
-        }
+        result = fibo(n);
 
         return result;
     }
 
-    public static void main(String[] args) {
-        MemoTest mm = new MemoTest();
+    private int fibo(int n) {
+        if(n < 2) return n; //0과 1은 그대로 return
+        if(arr[n] != 0) return arr[n] % 1234567;
 
-        System.out.println(mm.solution("my name is daramG"));
-
+        return arr[n] = (fibo(n - 1) + fibo(n -2)) % 1234567;
     }
 
 
+
+    public static void main(String[] args) throws IOException {
+        }
 }
+
+/* 구하고자하는 피보나치 수의 최댓값이 100,000 이므로 10을 100,101 크기만큼의
+   배열을 생성해두고 값을 저장하는데 사용한다.
+
+   - 피보나치 수를 구하는 함수 작성
+   피보나치 수 0 과 1의 값은 그대로 0과 1이므로 n이 2보다 작은 경우에는
+   바로 n 값을 return
+
+   만약 data 배열에 이전에 구현한 n 값이 있다면 해당 값을 1234567로
+   나눈 값을 return
+
+   처음으로 구하는 값이라면 값을 return하는 동시에 data 배열에 저장
+
+
+* */
+
+
+
+
+
+
 
 
 
