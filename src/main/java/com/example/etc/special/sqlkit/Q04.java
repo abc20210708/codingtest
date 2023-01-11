b72459f6c661144dc30ecb252013ba1e5854b6d8
abc20210708 <gotqlc2670@naver.com>
2023-01-11 16:21:36 +0900

    [SQL] lv1. 상위 n개 레코드

---
+++ src/main/java/com/example/etc/special/sqlkit/Q04.java
@@ -0,0 +1,15 @@
+package com.example.etc.special.sqlkit;
+
+public class Q04 {
+    /*
+    SELECT NAME
+    FROM ANIMAL_INS
+    WHERE DATETIME = (SELECT MIN(DATETIME)
+                      FROM ANIMAL_INS)
+    * */
+}
+/* 상위 n개 레코드
+    동물 보호소에 가장 먼저 들어온
+    동물의 이름을 조회하는 SQL 문을 작성해주세요.
+
+* */
