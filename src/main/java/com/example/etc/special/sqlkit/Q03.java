84a4024a0672b87ace24d0bdc6a2dab1326a5550
abc20210708 <gotqlc2670@naver.com>
2023-01-11 16:08:58 +0900

    [SQL] lv1. 아픈 동물 찾기

---
+++ src/main/java/com/example/etc/special/sqlkit/Q03.java
@@ -0,0 +1,28 @@
+package com.example.etc.special.sqlkit;
+
+public class Q03 {
+    /*
+    SELECT ANIMAL_ID, NAME
+    FROM ANIMAL_INS
+    WHERE INTAKE_CONDITION IN ('Sick')
+    ORDER BY ANIMAL_ID
+    * */
+}
+
+/*
+ SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID
+* */
+
+
+/*
+    아픈 동물 찾기
+
+    동물 보호소에 들어온 동물 중 아픈 동물의
+     아이디와 이름을 조회하는 SQL 문을 작성해주세요.
+     이때 결과는 아이디 순으로 조회해주세요.
+
+예시
+* */
