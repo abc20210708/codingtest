220002ec5a6a86a32719ca62dafa2fcddf45c622
abc20210708 <gotqlc2670@naver.com>
2023-01-12 18:16:29 +0900

    Update Q06.java

--- src/main/java/com/example/programmers/lv2/v00/Q06.java
+++ src/main/java/com/example/programmers/lv2/v00/Q06.java
@@ -50,6 +50,30 @@ public class Q06 {
 }
 * */
 
+/* 다른 풀이 2
+ import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String[] c: clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) +1);
+        }
+
+        Iterator<Integer> it = map.values().iterator();
+        while(it.hasNext()) {
+            answer *= it.next().intValue()+1;
+        }
+
+
+        return answer -1;
+    }
+}
+* */
+
 /*
 위장
 문제 설명
