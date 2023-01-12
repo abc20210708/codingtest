65c038639a595b2eacc6784c455ac42985b71a03
abc20210708 <gotqlc2670@naver.com>
2023-01-12 19:42:00 +0900

    Update Q05.java

--- src/main/java/com/example/programmers/lv2/v00/Q05.java
+++ src/main/java/com/example/programmers/lv2/v00/Q05.java
@@ -5,22 +5,24 @@ public class Q05 {
 
     class Solution {
         public String solution(int[] numbers) {
-            String answer = "0";
             String[] str = new String[numbers.length];
 
-            for (int i = 0; i < numbers.length; i++) {
-                str[i] = String.valueOf(numbers[i]);
+            for (int i = 0; i < str.length; i++) {
+                str[i] = Integer.toString(numbers[i]);
             }
 
-            Arrays.sort(str, (a, b) -> (b + a).compareTo(a + b));
-            if(!str[0].equals("0")) {
-                StringBuilder sb = new StringBuilder();
-                for(String s : str) {
-                    sb.append(s);
-                }
-                answer = sb.toString();
-            }
-            return answer;
+            Arrays.sort(str, ((o1, o2) -> (o2 + o1).compareTo(o1 + o2)));
+
+            if(str[0].equals("0"))
+                return "0";
+
+            StringBuilder answer = new StringBuilder();
+
+            for(String s : str)
+                answer.append(s);
+
+            return answer.toString();
+
         }
     }
 }
@@ -84,6 +86,26 @@ class Solution {
 
 
         return answer;
+* */
+
+/* 다른 풀이 3
+String answer = "0";
+            String[] str = new String[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++) {
+                str[i] = String.valueOf(numbers[i]);
+            }
+
+            Arrays.sort(str, (a, b) -> (b + a).compareTo(a + b));
+            if(!str[0].equals("0")) {
+                StringBuilder sb = new StringBuilder();
+                for(String s : str) {
+                    sb.append(s);
+                }
+                answer = sb.toString();
+            }
+            return answer;
+
 * */
 
 //참고 블로그
