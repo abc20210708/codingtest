e93f052f1b3be25319d667895f650203a57d7e3f
abc20210708 <gotqlc2670@naver.com>
2023-01-13 23:54:58 +0900

    Update Q22.java

--- src/main/java/com/example/programmers/lv2/v20/Q22.java
+++ src/main/java/com/example/programmers/lv2/v20/Q22.java
@@ -33,6 +33,27 @@ public class Q22 {
 
 }
 
+/* 다른 풀이
+int answer = 0;
+        for(int i = 0; i < babblings.length; i++) {
+            if(babblings[i].contains("ayaaya") || babblings[i].contains("yeye") ||
+               babblings[i].contains("woowoo") || babblings[i].contains("mama")) {
+                continue;
+            }
+
+            babblings[i] = babblings[i].replace("aya", " ");
+            babblings[i] = babblings[i].replace("ye", " ");
+            babblings[i] = babblings[i].replace("woo", " ");
+            babblings[i] = babblings[i].replace("ma", " ");
+            babblings[i] = babblings[i].replace(" ", "");
+
+            if(babblings[i].length()  == 0) answer++;
+
+        }
+        return answer;
+
+* */
+
 /*
 옹알이 (2)
 문제 설명
