e92191ec9d7e454d897a7239a26d7f7560e2ad65
abc20210708 <gotqlc2670@naver.com>
2023-01-14 14:33:24 +0900

    Update MemoTest.java

--- src/main/java/com/example/etc/MemoTest.java
+++ src/main/java/com/example/etc/MemoTest.java
@@ -1,17 +1,15 @@
 package com.example.etc;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 //
 public class MemoTest {
-
-
-
-
-
     public static void main(String[] args) throws IOException {
 
-        }
+
+
+    }
 }
 
 /*
