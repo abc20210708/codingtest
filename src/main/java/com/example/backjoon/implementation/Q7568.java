618c185bdad4088338223b7c7c02249e67780efc
abc20210708 <gotqlc2670@naver.com>
2023-01-15 15:40:46 +0900

    Update Q7568.java

--- src/main/java/com/example/backjoon/implementation/Q7568.java
+++ src/main/java/com/example/backjoon/implementation/Q7568.java
@@ -1,11 +1,46 @@
 package com.example.backjoon.implementation;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Q7568 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        // 배열 생성
+        int[][] arr = new  int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] =  Integer.parseInt(st.nextToken()); //무게
+            arr[i][1] =  Integer.parseInt(st.nextToken()); //키
+        }
+
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+            for (int j = 0; j < N; j++) {
+                if(i == j) continue;
+
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])
+                    rank++;
+
+            }
+            System.out.println(rank);
+        }
+
+    }
+}
+
+
+/* 다른 풀이
+Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
 
@@ -27,9 +62,7 @@ public class Q7568 {
             System.out.println(rank);
         }
 
-    }
-}
-
+* */
 
 /* 덩치
 덩치
