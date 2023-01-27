8a875e84f98c1f514f4a45075cd2e4ff086d5b16
abc20210708 <gotqlc2670@naver.com>
2023-01-27 12:01:36 +0900

    2460 : 지능형 기차 2

--- src/main/java/com/example/backjoon/implementation/Q2460.java
+++ src/main/java/com/example/backjoon/implementation/Q2460.java
@@ -5,19 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//지능형 기차 2 https://www.acmicpc.net/problem/2460
 public class Q2460 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br
             = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int[][] arr = new int[10][2];
+        StringTokenizer st;
 
-        for (int i = 0; i < arr.length; i++) {
-            
+        int[] up = new int[10];
+        int[] down = new int[10];
+
+        int person = 0;
+        int max = 0;
+
+        for (int i = 0; i < up.length; i++) {
+            st =  new StringTokenizer(br.readLine());
+            down[i] = Integer.parseInt(st.nextToken());
+            up[i] = Integer.parseInt(st.nextToken());
+
+            person -=  down[i];
+            person +=  up[i];
+
+            if(person > max) max = person;
         }
+
+        System.out.println(max);
 
     }
 }
